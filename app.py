fee1203109731c014bd5233a56ee3ea48e8dd79c
R406M <orozcorams406@gmail.com>
2025-01-13 22:49:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -234,4 +234,4 @@ def adjust_to_increment(amount, increment):
     return round(amount / increment) * increment
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug
+    app.run(host='0.0.0.0', port=5000, debug=True)
