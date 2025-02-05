3f65a7dcd5cd000874c57683dbb70ee117c5b2af
R406M <orozcorams406@gmail.com>
2025-02-05 17:17:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -222,4 +222,5 @@ def webhook():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Render asigna el puerto en la variable PORT
+    app.run(host='0.0.0.0', port=port)
